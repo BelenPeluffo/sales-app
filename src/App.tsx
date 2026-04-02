4f0774a4c5904f454651e0116ae3c26428c67e55
Belén Peluffo <belenpeluffolupiano@gmail.com>
2026-04-02 18:43:50 -0300

    feat: implementación de página de login

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import { Header } from "./modules/common";
+// import { Header } from "./modules/common";
+
+import { Login } from "./modules/auth";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
+    <div className="w-screen h-screen">
+      {/* <Header /> */}
+      <Login />
     </div>
   );
 }
---
+++ src/modules/auth/layouts/login.tsx
@@ -0,0 +1,10 @@
+const Login = () => {
+  return (
+    <div className="flex flex-col flex-wrap gap-2 w-full h-full text-center justify-center content-center">
+      <h1 className="font-medium text-5xl">Bienvenidx</h1>
+      <button className="border rounded w-[25%] hover:cursor-pointer hover:bg-green-300">Abrir sesión</button>
+    </div>
+  );
+};
+
+export default Login;
