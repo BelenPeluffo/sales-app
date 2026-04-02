c40e52126d88896c5a2850e3623563aabf887aaf
Belén Peluffo <belenpeluffolupiano@gmail.com>
2026-04-02 19:31:34 -0300

    feat: instalación de componentes y tema de shadcn

--- src/modules/auth/layouts/login.tsx
+++ src/modules/auth/layouts/login.tsx
@@ -1,8 +1,40 @@
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/modules/common/components/shadcn/dialog";
+
 const Login = () => {
   return (
     <div className="flex flex-col flex-wrap gap-2 w-full h-full text-center justify-center content-center">
       <h1 className="font-medium text-5xl">Bienvenidx</h1>
-      <button className="border rounded w-[25%] hover:cursor-pointer hover:bg-green-300">Abrir sesión</button>
+      <Dialog>
+        <DialogTrigger asChild>
+          <button className="border-black border-1 rounded w-[25%] hover:cursor-pointer hover:bg-green-300">
+            Abrir sesión
+          </button>
+        </DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Iniciar sesión</DialogTitle>
+          </DialogHeader>
+          Khe
+          <DialogFooter>
+            <DialogClose asChild>
+              <button className="border-black border-1 rounded w-[25%] hover:cursor-pointer hover:bg-green-300">
+                Cancelar
+              </button>
+            </DialogClose>
+            <button className="border-black border-1 rounded w-[25%] hover:cursor-pointer bg-green-300 hover:shadow-[0_0_30px_rgb(134,239,172,0.6)]">
+              Ingresar
+            </button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 };
