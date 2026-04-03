2fa37fcd4c61f16523b46087baed4fe6bc07fce4
Belén Peluffo <belenpeluffolupiano@gmail.com>
2026-04-02 22:10:13 -0300

    feat: agrega componente Table y su implementación en la vista de sesión

--- src/modules/cierres/layouts/session.tsx
+++ src/modules/cierres/layouts/session.tsx
@@ -1,10 +1,29 @@
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/modules/common/components/shadcn/table";
 import Header from "./header";
+
+const MOCK_MOVIMIENTOS = [
+  {
+    time: "10:50",
+    total: 35760,
+  },
+  {
+    time: "11:50",
+    total: 30760,
+  },
+];
 
 const Session = () => {
   return (
     <div className="w-full">
       <Header />
-      <div className="flex flex-row flex-wrap justify-between mt-6 w-full content-center">
+      <div className="flex flex-row flex-wrap justify-between mt-6 mb-3 w-full content-center">
         <p className="font-medium text-xl w-[25%]">Transacciones del día</p>
         <div className="flex gap-1 w-fit flex-wrap content-center">
           <button className="border-black border-1 h-fit px-1 rounded hover:cursor-pointer bg-green-300 hover:ring-2 hover:ring-green-300">
@@ -15,6 +34,27 @@ const Session = () => {
           </button>
         </div>
       </div>
+      <Table>
+        {/** TODO: implementar gestión de tabla con react-query */}
+        <TableHeader>
+          <TableRow>
+            <TableHead>Hora</TableHead>
+            <TableHead>Monto total</TableHead>
+            <TableHead>Acciones</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+            {MOCK_MOVIMIENTOS.map((movimiento) => {
+                return (
+                    <TableRow>
+                        <TableCell>{movimiento.time}</TableCell>
+                        <TableCell>ARS $ {movimiento.total}</TableCell>
+                        <TableCell>Acciones</TableCell>
+                    </TableRow>
+                )
+            })}
+        </TableBody>
+      </Table>
     </div>
   );
 };
