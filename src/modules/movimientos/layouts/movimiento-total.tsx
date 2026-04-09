5095aaaf85f4602d9cbcaa346c6a0c726c8f6ea8
Belén Peluffo <belenpeluffolupiano@gmail.com>
2026-04-09 05:04:28 -0300

    feat (movimiento-total): implementa breakdown de ingresos y egresos

--- src/modules/movimientos/layouts/movimiento-total.tsx
+++ src/modules/movimientos/layouts/movimiento-total.tsx
@@ -1,4 +1,6 @@
 import { useFormContext, useWatch } from "react-hook-form";
+import type { Payment } from "../types";
+import { TRANSACTION_TYPES } from "../constants";
 
 const MovimientoTotal = () => {
   const { control } = useFormContext();
@@ -6,7 +8,28 @@ const MovimientoTotal = () => {
     control,
     name: `total`,
   });
-  return <div>Total: {grandTotal}</div>;
+  const payments: Array<Payment> = useWatch({
+    control,
+    name: "payments",
+  });
+  const { ingresos, egresos } = payments.reduce(
+    (total, payment) => {
+      if (
+        payment.transactionType === undefined ||
+        payment.transactionType === TRANSACTION_TYPES.IN
+      )
+        return { ...total, ingresos: total.ingresos + payment.subtotal };
+      return { ...total, egresos: total.egresos + payment.subtotal };
+    },
+    { ingresos: 0, egresos: 0 },
+  );
+  return (
+    <div className="flex flex-col justify-center content-center text-center w-full">
+      <p>Ingresos: {ingresos}</p>
+      <p>Egresos: {egresos} </p>
+      <p>Total: {grandTotal}</p>
+    </div>
+  );
 };
 
 export default MovimientoTotal;
