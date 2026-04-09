492e28a6cd70ebc085bb1b947f89ee7e8c898503
Belén Peluffo <belenpeluffolupiano@gmail.com>
2026-04-09 04:39:56 -0300

    refactor (monto-input): agrega variables para mostrar tooltip y calcular deshabilitación de input

--- src/modules/movimientos/components/monto-input.tsx
+++ src/modules/movimientos/components/monto-input.tsx
@@ -9,6 +9,11 @@ import { Input } from "@/modules/common/components/shadcn/input";
 import { usePaymentItemStore } from "../stores";
 import { CASH_METHODS } from "../constants";
 import type { PaymentItemType } from "../types";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/modules/common/components/shadcn/tooltip";
 
 const MontoInput = ({ index }: Pick<PaymentItemType, "index">) => {
   const { control, setValue } = useFormContext();
@@ -21,7 +26,10 @@ const MontoInput = ({ index }: Pick<PaymentItemType, "index">) => {
     control,
     name: "total",
   });
-  const isMontoDisabled = paymentType && CASH_METHODS.includes(paymentType);
+  const isMethodCash = paymentType && CASH_METHODS.includes(paymentType);
+  const isMethodUndefined = !paymentType;
+  const isMontoDisabled = isMethodUndefined || isMethodCash;
+
   return (
     <Controller
       name={`payments.${index}.subtotal`}
@@ -34,19 +42,28 @@ const MontoInput = ({ index }: Pick<PaymentItemType, "index">) => {
               <span className="absolute left-3 top-1/2 -translate-y-1/2">
                 {currency ? getCurrencySymbol(currency) : "AR$"}
               </span>
-              <Input
-                type="number"
-                className={currency || "pesosAr"}
-                {...field}
-                onChange={(event) => {
-                  const subtotal = Number(event.target.value);
-                  field.onChange(subtotal);
-                  setValue("total", total + subtotal);
-                }}
-                min={0}
-                disabled={isMontoDisabled}
-                aria-invalid={fieldState.invalid}
-              />
+              <Tooltip>
+                <TooltipTrigger>
+                  <Input
+                    type="number"
+                    className={currency || "pesosAr"}
+                    {...field}
+                    onChange={(event) => {
+                      const subtotal = Number(event.target.value);
+                      field.onChange(subtotal);
+                      setValue("total", total + subtotal);
+                    }}
+                    min={0}
+                    disabled={isMontoDisabled}
+                    aria-invalid={fieldState.invalid}
+                  />
+                </TooltipTrigger>
+                {isMethodUndefined ? (
+                  <TooltipContent>
+                    Defina un método de pago para editar
+                  </TooltipContent>
+                ) : null}
+              </Tooltip>
             </div>
             {fieldState.invalid ? (
               <FieldError errors={[fieldState.error]} className="text-end" />
