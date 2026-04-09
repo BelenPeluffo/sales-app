c47c4e0920ef895af957c8d3f210f0c5ffca43f0
Belén Peluffo <belenpeluffolupiano@gmail.com>
2026-04-09 03:29:47 -0300

    feat: instala tooltip de shadCN

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import { TooltipProvider } from "./modules/common/components/shadcn/tooltip";
 import { AppRouter } from "./modules/common/routes";
 
 function App() {
   return (
     <div className="w-screen h-screen p-2">
-      <AppRouter />
+      <TooltipProvider>
+        <AppRouter />
+      </TooltipProvider>
     </div>
   );
 }
